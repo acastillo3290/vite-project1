912e4c79df202384b949453e9750b2de9bfb1189
Aaron C <aaronc@MacBook-Pro-5.local>
2023-12-09 09:12:06 -0500

    local storage issue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 import './style.css';
-import palettes from './palettes.json';
-import { handleSubmit } from './mods/util'
+import { getPalettes } from './mods/util';
+import { setLocalStorageKey, getLocalStorageKey } from './local-storage';
+import { uuidv4 } from './mods/util';
+const form = document.querySelector('#form');
 
+const handleSubmit = (e) => {
+    e.preventDefault();
 
+    const formObj = Object.fromEntries(new FormData(e.target));
+    console.log(formObj);
+    addValue(formObj);
+
+};
+
+const getValue = (key) => getLocalStorageKey(key);
+const setValue = (key, names) => setLocalStorageKey(key, names);
+
+const addValue = (value) => {
+    const keyId = uuidv4
+    const values = getValue(keyId);
+    setValue(keyId, [...values, value]);
+};
 
 const main = () => {
+    getPalettes();
     form.addEventListener('submit', handleSubmit);
 };
 main();
--- src/mods/util.js
+++ src/mods/util.js
@@ -1,12 +1,33 @@
-const userPalettes = document.querySelector('#user-palettes')
+import palettes from '../palettes.json';
+const userPalettes = document.querySelector('#user-palettes');
 
-export const handleSubmit = (e) => {
-    e.preventDefault();
+export const getPalettes = (e) => {
+    // e.preventDefault();
 
-    const formObj = Object.fromEntries(new FormData(e.target));
-    const card = document.createElement('div');
-    const title = document.createElement('h1');
-    
-    card.append(title);
-    userPalettes.append(card);
+    palettes.forEach(element => {
+        const div = document.createElement('div');
+    div.className = 'default-div';
+    div.innerHTML = `
+    <li>
+    <h1>${element.title}</h1>
+      <br>
+      <div style="background: ${element.colors[0]}">Text Example</div>
+      <button>Copy ${element.colors[0]}</button>
+      <br>
+      <div style="background: ${element.colors[1]}">Text Example</div>
+      <button>Copy ${element.colors[1]}</button>
+      <br>
+      <div style="background: ${element.colors[2]}">Text Example</div>
+      <button>Copy ${element.colors[2]}</button>
+      <button>Delete</button>
+      <div>${element.temperature}</div>
+    </li>`;
+        userPalettes.append(div);
+    });
+};
+
+export const uuidv4 = () => {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
 };
